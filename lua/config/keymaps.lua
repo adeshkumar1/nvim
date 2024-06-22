a2c8f14f046b601a29b36e08cfffe00ee4145d80
Adesh Kumar <89107712+adeshkumar1@users.noreply.github.com>
2024-06-22 12:26:21 -0500

    my nvim setup

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,23 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Toggle Tree
+keymap.set("n", "<C-n>", ":Neotree toggle<Return>", opts)
+
+---Select all
+keymap.set("n", "<C-a>", "ggVG")
+
+---Go To Next Diagnostic
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
+
+--Code Folding
+keymap.set("n", "<leader>kl", "za", opts)
