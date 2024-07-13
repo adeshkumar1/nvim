88ae0efa0fa2aba8f7b3563450b9ce44b0f69dba
Adesh Kumar <89107712+adeshkumar1@users.noreply.github.com>
2024-07-13 16:59:32 -0500

    small updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,9 @@
 return {
-   {
-      "catppuccin/nvim",
-      name = "catppuccin",
-      priority = 1000,
-      opts = function()
-         require("catppuccin").setup({
-            flavour = "latte",
-            transparent_background = true,
-         })
-      end,
+   "catppuccin/nvim",
+   name = "catppuccin",
+   priority = 1000,
+   opts = {
+      flavour = "mocha",
+      transparent_background = true,
    },
 }
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,5 +1,5 @@
 return {
+   enabled = true,
    "stevearc/conform.nvim",
-   disabled = true,
    opts = {},
 }
