f35fad178582b42a20f07bdebaa4f441e699a59d
Adesh Kumar <89107712+adeshkumar1@users.noreply.github.com>
2025-01-15 14:43:05 -0600

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,21 @@
 return {
-   "nvim-treesitter/nvim-treesitter",
-   opts = {
-      ensure_installed = {
-         "bash",
-         "html",
-         "javascript",
-         "json",
-         "lua",
-         "python",
-         "tsx",
-         "typescript",
-         "vim",
-         "yaml",
-         "tsx",
-      },
-   },
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "python",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+      "tsx",
+      "cpp",
+      "make",
+      "cmake",
+    },
+  },
 }
