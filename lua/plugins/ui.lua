1ab9416be3dac83e9a08b07b2a9355433b16212e
Adesh Kumar <adeshk2@illinois.edu>
2025-01-28 23:20:15 -0600

    fixed animation

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -33,49 +33,49 @@ return {
       },
     },
   },
-  {
-    "echasnovski/mini.animate",
-    recommended = true,
-    event = "VeryLazy",
-    version = "*",
-    opts = function()
-      -- don't use animate when scrolling with the mouse
-      local mouse_scrolled = false
-      for _, scroll in ipairs({ "Up", "Down" }) do
-        local key = "<ScrollWheel" .. scroll .. ">"
-        vim.keymap.set({ "", "i" }, key, function()
-          mouse_scrolled = true
-          return key
-        end, { expr = true })
-      end
-
-      local animate = require("mini.animate")
-      return {
-        resize = {
-          timing = animate.gen_timing.linear({ duration = 0, unit = "total" }),
-        },
-        scroll = {
-          timing = animate.gen_timing.linear({ duration = 0, unit = "total" }),
-          subscroll = animate.gen_subscroll.equal({
-            predicate = function(total_scroll)
-              if mouse_scrolled then
-                mouse_scrolled = false
-                return false
-              end
-              return total_scroll > 1
-            end,
-          }),
-        },
-        cursor = {
-          enable = false,
-        },
-        open = {
-          enable = false,
-        },
-        close = {
-          enable = false,
-        },
-      }
-    end,
-  },
+  -- {
+  --   "echasnovski/mini.animate",
+  --   recommended = true,
+  --   event = "VeryLazy",
+  --   version = "*",
+  --   opts = function()
+  --     -- don't use animate when scrolling with the mouse
+  --     local mouse_scrolled = false
+  --     for _, scroll in ipairs({ "Up", "Down" }) do
+  --       local key = "<ScrollWheel" .. scroll .. ">"
+  --       vim.keymap.set({ "", "i" }, key, function()
+  --         mouse_scrolled = true
+  --         return key
+  --       end, { expr = true })
+  --     end
+  --
+  --     local animate = require("mini.animate")
+  --     return {
+  --       resize = {
+  --         timing = animate.gen_timing.linear({ duration = 0, unit = "total" }),
+  --       },
+  --       scroll = {
+  --         timing = animate.gen_timing.linear({ duration = 0, unit = "total" }),
+  --         subscroll = animate.gen_subscroll.equal({
+  --           predicate = function(total_scroll)
+  --             if mouse_scrolled then
+  --               mouse_scrolled = false
+  --               return false
+  --             end
+  --             return total_scroll > 1
+  --           end,
+  --         }),
+  --       },
+  --       cursor = {
+  --         enable = false,
+  --       },
+  --       open = {
+  --         enable = false,
+  --       },
+  --       close = {
+  --         enable = false,
+  --       },
+  --     }
+  --   end,
+  -- },
 }
