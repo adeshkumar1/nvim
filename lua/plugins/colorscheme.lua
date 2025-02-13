5dd16f4842aeb7d5ec5dffc2ab962f3b9201402a
Adesh Kumar <89107712+adeshkumar1@users.noreply.github.com>
2025-02-13 09:53:40 -0600

    changed to catpuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "storm",
-      transparent = false,
-      on_colors = function(_) end,
-      on_highlights = function(_) end,
-    })
-    vim.cmd("colorscheme tokyonight")
-  end,
+  -- "folke/tokyonight.nvim",
+  -- priority = 1000,
+  -- config = function()
+  --   require("tokyonight").setup({
+  --     style = "storm",
+  --     transparent = false,
+  --     on_colors = function(_) end,
+  --     on_highlights = function(_) end,
+  --   })
+  --   vim.cmd("colorscheme tokyonight")
+  -- end,
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
